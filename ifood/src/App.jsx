f1ecbf99a1972f1b72cd40061ae3962a71a67bd8
Caiolib <caioliberal@gmail.com>
2024-05-02 18:50:36 -0300

    Reiniciei

---
+++ ifood/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from "./components/ui/button"
+function App() {
+  
+
+  return (
+    <>
+    <div className="px-20"> 
+      <Button>Hello</Button>
+    </div>
+    
+    </>
+     
+  )
+}
+
+export default App
